216fa55715ac3b79e9a63c005ba2792d73522095
David Webster <dwebster@appacademy.io>
2018-01-14 22:38:40 -0500

    Begin refactoring of player

--- frontend/components/featured_playlists/GenericMusicIndex.jsx
+++ frontend/components/featured_playlists/GenericMusicIndex.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-// import MusicIndexItem from './music_index_item';
 import { withRouter } from 'react-router-dom';
 
 const GenericMusicIndex = (props) => {
   const { IndexItem } = props;
-  
+
   return(
     <ul className='playlist-items'>
       { props.indexItems.map(item => (
